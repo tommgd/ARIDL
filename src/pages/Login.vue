c76588ac4141160ff80b6911fcfc2ff36bbfeede
KrisGra <39800337+krisgrant@users.noreply.github.com>
2023-07-11 23:32:55 +0300

    Discord User Data Sync
    - made the button and code to sync discord user data
    - also the code to fetch a new access token if it's expired
    let's just hope it works

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,34 +2,88 @@
 import Spinner from "../components/Spinner.vue";
 import { pb } from "../pocketbase.js";
 
+console.log("what", localStorage.getItem("access_token"));
+
+const UpdateUserData = async () => {
+	const userResponse = await fetch("https://discord.com/api/v10/users/@me", {
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+		},
+	});
+	const userData = await userResponse.json();
+
+	await pb.collection("users").update(pb.authStore.model.id, {
+		global_name: userData.global_name,
+		avatar_url: `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}`,
+		discord_id: userData.id,
+		banner: userData.banner
+			? `https://cdn.discordapp.com/banners/${userData.id}/${userData.banner}`
+			: null,
+		banner_color: userData.banner_color,
+	});
+};
+
 const Login = async () => {
 	const authData = await pb.collection("users").authWithOAuth2({
 		provider: "discord",
-		redirectUrl:
-			"https://discord.com/api/v10/oauth2/authorize?client_id=1128066454768599100&redirect_uri=http%3A%2F%2F157.245.244.129%2Fapi%2Foauth2-redirect&response_type=code&scope=identify%20email%20connections%20guilds%20guilds.members.read",
-		createData: {
-			permissions: "member",
-		},
+		// redirectUrl:
+		// 	"https://discord.com/api/v10/oauth2/authorize?client_id=1128066454768599100&redirect_uri=http%3A%2F%2F157.245.244.129%2Fapi%2Foauth2-redirect&response_type=code&scope=identify%20email%20connections%20guilds%20guilds.members.read",
+		// createData: {
+		// 	permissions: "member",
+		// },
 	});
 
-	// const discordResponse = await fetch("https://discord.com/api/v10/users/@me", {
-	// 	headers: {
-	// 		Authorization: `Bearer ${authData.meta.accessToken}`,
-	// 	},
-	// });
-	// const discordData = await discordResponse.json();
-
 	const discordData = await authData.meta.rawUser;
+	console.log(authData.meta);
 
 	await pb.collection("users").update(pb.authStore.model.id, {
 		global_name: discordData.global_name,
-		avatar_url: `https://cdn.discordapp.com/avatars/${discordData.id}/${discordData.avatar}`,
+		avatar_url: authData.meta.avatarUrl,
 		discord_id: discordData.id,
 		banner: discordData.banner
 			? `https://cdn.discordapp.com/banners/${discordData.id}/${discordData.banner}`
 			: null,
 		banner_color: discordData.banner_color,
 	});
+
+	localStorage.setItem("access_token", authData.meta.accessToken);
+	localStorage.setItem("refresh_token", authData.meta.refreshToken);
+};
+
+const SyncData = async () => {
+	try {
+		await UpdateUserData();
+	} catch (e) {
+		if (e.message == "401: Unauthorized") {
+			try {
+				const tokenResponse = await fetch(
+					"https://discord.com/api/oauth2/token",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/x-www-form-urlencoded",
+						},
+						body: new URLSearchParams({
+							client_id: "1128066454768599100",
+							client_secret: "aiZtvYylu-5hiUBCXqKpNhdgs2tGgvU9",
+							grant_type: "refresh_token",
+							refresh_token: localStorage.getItem("refresh_token"),
+						}),
+					},
+				);
+				const tokenData = await tokenResponse.json();
+
+				await localStorage.setItem("access_token", tokenData.access_token);
+				await localStorage.setItem("refresh_token", tokenData.refresh_token);
+
+				await UpdateUserData();
+			} catch (e) {
+				alert(e);
+			}
+		} else {
+			alert("Unknown error occurred", e);
+		}
+	}
 };
 </script>
 
@@ -38,12 +92,18 @@ const Login = async () => {
 		<Spinner></Spinner>
 	</main>
 	<main v-else>
-		<button @click="Login">Login with Discord OAuth2</button>
+		<button class="Login" @click="Login">Login with Discord OAuth2</button>
+		<button class="SyncData" @click="SyncData">
+			Sync Discord Profile Data
+		</button>
 	</main>
 </template>
 
 <style>
-button {
+.Login {
 	background-color: teal;
+}
+.SyncData {
+	background-color: purple;
 }
 </style>
